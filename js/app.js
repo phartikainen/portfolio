ee34b0d5612a84b682099169fd89b4c94abe6955
Pekka Hartikainen <pekka.t.hartikainen@gmail.com>
2014-05-06 17:23:23 +0300

    Some type testing

--- js/app.js
+++ js/app.js
@@ -8,10 +8,26 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers',
-  'myApp.placeholders'
+  'myApp.placeholders',
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+// Small caps paragraphs
+
+ $.fn.wrapStart = function (numWords) { 
+    var node = this.contents().filter(function () { return this.nodeType == 3 }).first(),
+        text = node.text(),
+        first = text.split(" ", numWords).join(" ");
+
+    if (!node.length)
+        return;
+    
+    node[0].nodeValue = text.slice(first.length);
+    node.before('<span>' + first + '</span>');
+};
+
+$("p").wrapStart(3);
